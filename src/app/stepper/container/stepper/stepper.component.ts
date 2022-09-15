a0feeccae9c33ed5ffa55009f2ccfac42a5129d0
elianyrobles <martinezeliany1996@gmail.com>
2022-09-15 19:59:38 -0400

    Added next on click and card animation

--- src/app/stepper/container/stepper/stepper.component.ts
+++ src/app/stepper/container/stepper/stepper.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { StepperService } from '../../services/stepper.service';
@@ -6,6 +6,7 @@ import { SizeOption } from '../../models';
 import { Store } from '@ngrx/store';
 import { saveSize } from '../../store/stepper.actions';
 import { getSize } from '../../store/stepper.selector';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-stepper',
@@ -25,13 +26,18 @@ export class StepperComponent {
     private _formBuilder: FormBuilder,
     private stepperSvc: StepperService,
     private store: Store
-    ) {
-      this.sizeOptions$ = this.stepperSvc.getAllSizes();
-    }
-
-  saveSize(event: string) {
-    this.store.dispatch(saveSize({ size: event}));
+  ) {
+    this.sizeOptions$ = this.stepperSvc.getAllSizes();
   }
 
+  saveSize(event: string) {
+    this.store.dispatch(saveSize({ size: event }));
+  }
+
+  goForward(stepper: MatStepper) {
+    setTimeout(function () {
+      stepper.next();
+    }, 1000);
+  }
 
 }
